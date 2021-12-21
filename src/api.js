5853482c4be41810b51973e04ee3dd9dbd2daad7
Darwin <77340894+charmful0x@users.noreply.github.com>
2021-12-21 13:51:31 +0200

    chore: imports prettified

--- src/api.js
+++ src/api.js
@@ -4,7 +4,11 @@ import { getArweaveSaves } from "./arweave-saves/api.js";
 import { getPublicDrives } from "./ardrive/api.js";
 import { getKoii } from "./koii/api.js";
 import { getPianitySongs } from "./pianity/api.js";
-import { getPermacast, getTotalPermacastSize } from "./permacast/api.js";
+import {
+  getPermacast,
+  getTotalPermacastSize,
+  getEpisodesOf,
+} from "./permacast/api.js";
 
 export async function getWeaveAggregator(network) {
   switch (network) {
