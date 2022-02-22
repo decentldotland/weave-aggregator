105cd851270b68dba6937b7e6f4d9695f403008d
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-02-22 17:41:32 +0200

    feat: feeds address filtering

--- src/arweave-saves/api.js
+++ src/arweave-saves/api.js
@@ -1,22 +1,28 @@
 import { querySchema, gqlTemplate } from "../utils/arweave/gql.js";
+import { arweaveSavePer } from "../address/gql.js";
 import { arweave } from "../utils/arweave/arweave.js";
 
-export async function getArweaveSaves() {
+export async function getArweaveSaves(address) {
   try {
     const savesArray = [];
-    const saves = await gqlTemplate(querySchema.saves.blogs);
+    let saves;
 
+    if (address) {
+      const query = arweaveSavePer(address);
+      saves = await gqlTemplate(query);
+    } else {
+      saves = await gqlTemplate(querySchema.saves.blogs);
+    }
     for (let tx of saves) {
-      const tags = tx["tags"]
+      const tags = tx["tags"];
       savesArray.push({
         sid: tx.id,
         poster: tx.owner,
-        url: tags.find(tag => tag.name === "page:url")?.value,
-        title: tags.find(tag => tag.name === "page:title")?.value,
-        timestamp: tx.timestamp
+        url: tags.find((tag) => tag.name === "page:url")?.value,
+        title: tags.find((tag) => tag.name === "page:title")?.value,
       });
     }
-    
+
     return savesArray;
   } catch (error) {
     console.log(`${error.name}: ${error.message}`);
