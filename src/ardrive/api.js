991afa6e6f44dee531f7e851fe1124e515a13313
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-03-03 20:41:02 +0200

    perf: error handling

--- src/ardrive/api.js
+++ src/ardrive/api.js
@@ -3,35 +3,34 @@ import { ardriveDrivesPer } from "../address/gql.js";
 import { arweave } from "../utils/arweave/arweave.js";
 
 export async function getPublicDrives(address) {
-  const feed = [];
+  try {
+    const feed = [];
 
-  let drives;
+    let drives;
 
-  if (address) {
-    const query = ardriveDrivesPer(address);
-    drives = await gqlTemplate(query);
-  } else {
-    drives = await gqlTemplate(querySchema.ardrive.drives);
+    if (address) {
+      const query = ardriveDrivesPer(address);
+      drives = await gqlTemplate(query);
+    } else {
+      drives = await gqlTemplate(querySchema.ardrive.drives);
+    }
+
+    for (let drive of drives) {
+      const driveId = drive["tags"].find(
+        (tag) => tag.name === "Drive-Id"
+      )?.value;
+
+      feed.push({
+        metadata: drive.id,
+        poster: drive.owner,
+        url: `https://app.ardrive.io/#/drives/${driveId}`,
+        timestamp: drive.timestamp,
+      });
+    }
+
+    return feed;
+  } catch (error) {
+    console.log(error);
   }
-
-  for (let drive of drives) {
-    const driveData = JSON.parse(
-      await arweave.transactions.getData(drive.id, {
-        decode: true,
-        string: true,
-      })
-    );
-
-    const driveId = drive["tags"].find((tag) => tag.name === "Drive-Id")?.value;
-
-    feed.push({
-      metadata: drive.id,
-      poster: drive.owner,
-      name: driveData.name,
-      url: `https://app.ardrive.io/#/drives/${driveId}`,
-      timestamp: drive.timestamp,
-    });
-  }
-
-  return feed;
 }
+
