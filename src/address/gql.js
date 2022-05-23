310dec05ba177337e161cad3cca3f4e2b382ecac
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-05-23 17:05:53 +0300

    fix: track the new Koii aNFT Src id

--- src/address/gql.js
+++ src/address/gql.js
@@ -6,7 +6,7 @@ export function koiiCollectiblePer(address) {
     tags: [
         { name: "Network", values: ["Koi", "Koii"]},
         { name: "Content-Type", values: ["image/png", "image/jpeg"]},
-        { name: "Contract-Src", values: ["I8xgq3361qpR8_DvqcGpkCYAUTMktyAgvkm6kGhJzEQ", "r_ibeOTHJW8McJvivPJjHxjMwkYfAKRjs-LjAeaBcLc"]}
+        { name: "Contract-Src", values: ["I8xgq3361qpR8_DvqcGpkCYAUTMktyAgvkm6kGhJzEQ", "r_ibeOTHJW8McJvivPJjHxjMwkYfAKRjs-LjAeaBcLc", "14l2t0DtenMRKPasR6Xi3pmQm3rqopD4cUr6Q5oD8lc"]}
         ]
     first: 30
   ) {
