9750228b19e4e50ea989482f897b6e3b947ab568
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-06-07 21:21:08 +0300

    feat: aggregate phaver.com

--- src/lens-protocol/api.js
+++ src/lens-protocol/api.js
@@ -3,20 +3,30 @@ import { lensFeed } from "./gql.js";
 export async function getLensFeed() {
   try {
     const feed = await lensFeed();
+
     // filter content broadcasted via lenster.xyz of type 'Post' only
     const filteredFeed = feed.filter(
-      (post) => post["__typename"] === "Post" && post.appId === "Lenster"
+      (post) =>
+        post["__typename"] === "Post" &&
+        (post.appId === "Lenster" || post.appId === "phaver")
     );
 
     for (const post of filteredFeed) {
       // remove unnecessary properties
       for (let key in post) {
-        if (!["id", "profile", "metadata", "createdAt"].includes(key)) {
+        if (
+          !["id", "profile", "metadata", "createdAt", "appId"].includes(key)
+        ) {
           delete post[key];
         }
         // add direct access for profile's page & pfp on lenster.xyz
-        post.profile.url = `https://lenster.xyz/u/${post.profile?.handle}`
+
+        post.profile.url =
+          post.appId === "Lenster"
+            ? `https://lenster.xyz/u/${post.profile?.handle}`
+            : `https://lensfrens.xyz/${post.profile.handle}`;
         post.profile.pfp = post.profile.picture?.original?.url;
+        post.origine = post.appId;
       }
     }
 
