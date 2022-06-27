dcf05da2650f2448424a0bd26b7ddb581cc2b635
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-06-27 21:31:22 +0300

    perf: remove TXs decoding

--- src/art-by-city/api.js
+++ src/art-by-city/api.js
@@ -1,54 +1,10 @@
 import { querySchema, gqlTemplate } from "../utils/arweave/gql.js";
 import { arweave } from "../utils/arweave/arweave.js";
 
-async function getArtTxs() {
-  try {
-    const txs = await gqlTemplate(querySchema.artbycity.art);
-
-    return txs;
-  } catch (error) {
-    console.log(`${error.name}: ${error.message}`);
-    process.exit(1);
-  }
-}
-
-async function getArtWorkMetadata(artTxs) {
-  try {
-    const feed = [];
-
-    for (let tx of artTxs) {
-      const metadata = await arweave.transactions.getData(tx.id, {
-        decode: true,
-        string: true,
-      });
-      const artObj = JSON.parse(metadata);
-
-      const art = {
-        aid: tx.id,
-        title: artObj?.title,
-        desc: artObj?.description,
-        slug: artObj?.slug,
-        creator: artObj?.creator,
-        creationDate: artObj?.published,
-        image: `https://arweave.net/${artObj?.images?.[0]?.["preview"]}`,
-      };
-
-      feed.push(art);
-    }
-
-    return feed;
-  } catch (error) {
-    console.log(`${error.name}: ${error.message}`);
-    process.exit(1);
-  }
-}
-
 export async function getArtByCity() {
   try {
-    const artsTxs = await getArtTxs();
-    const feed = await getArtWorkMetadata(artsTxs);
-
-    return feed;
+    const txs = await gqlTemplate(querySchema.artbycity.art);
+    return txs;
   } catch (error) {
     console.log(`${error.name}: ${error.message}`);
     process.exit(1);
