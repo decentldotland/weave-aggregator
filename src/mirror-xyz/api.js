7602dcf0b9242b2ea4f50fc4b2c59f8d552fd3b1
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-06-27 19:42:38 +0300

    perf: remove data retrieving components

--- src/mirror-xyz/api.js
+++ src/mirror-xyz/api.js
@@ -1,10 +1,9 @@
 import { querySchema, gqlTemplate } from "../utils/arweave/gql.js";
 import { arweave } from "../utils/arweave/arweave.js";
 
-async function getBlogsTxs() {
+export async function getMirror() {
   try {
     const txs = await gqlTemplate(querySchema.mirror.blogs);
-
     return txs;
   } catch (error) {
     console.log(`${error.name}: ${error.message}`);
@@ -12,42 +11,4 @@ async function getBlogsTxs() {
   }
 }
 
-async function getMirrorBlogMetadata(blogsTxs) {
-  try {
-    const feed = [];
 
-    for (let tx of blogsTxs) {
-      const metadata = await arweave.transactions.getData(tx.id, {
-        decode: true,
-        string: true,
-      });
-      const blogObj = JSON.parse(metadata);
-
-      const blog = {
-        bid: tx.id,
-        title: blogObj.content.title,
-        timestamp: blogObj.content.timestamp,
-        poster: blogObj.authorship.contributor,
-      };
-
-      feed.push(blog);
-    }
-
-    return feed;
-  } catch (error) {
-    console.log(`${error.name}: ${error.message}`);
-    process.exit(1);
-  }
-}
-
-export async function getMirror() {
-  try {
-    const blogsTxs = await getBlogsTxs();
-    const feed = await getMirrorBlogMetadata(blogsTxs);
-    
-    return feed;
-  } catch (error) {
-    console.log(`${error.name}: ${error.message}`);
-    process.exit(1);
-  }
-}
