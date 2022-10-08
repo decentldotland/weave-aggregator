26136d3e1609c8bc79400998bd5ddb27a8717d52
Darwin <77340894+charmful0x@users.noreply.github.com>
2022-10-08 14:39:22 +0200

    chore: restructure permapages APIs & prettify

---
+++ src/permapages/stamps/api.js
@@ -0,0 +1,35 @@
+import { makeQueries } from "./gql.js";
+import uniqBy from "lodash.uniqby";
+
+export async function getStamps(address) {
+  try {
+    const feed = [];
+    const stamps = await makeQueries();
+
+    for (let stamp of stamps) {
+      const inputTag = JSON.parse(
+        stamp["tags"].find((tag) => tag.name === "Input")?.value
+      );
+
+      if (inputTag.function === "stamp") {
+        feed.push({
+          stampedAsset: inputTag?.transactionId,
+          stamper: stamp["tags"].find((tag) => tag.name === "Sequencer-Owner")
+            ?.value,
+          timestamp: inputTag?.timestamp,
+        });
+      }
+    }
+
+    if (address) {
+      return uniqBy(
+        feed.filter((stamp) => stamp.stamper === address),
+        (n) => n.stampedAsset
+      );
+    }
+
+    return uniqBy(feed, (n) => n.stampedAsset);
+  } catch (error) {
+    console.log(`${error.name} : ${error.message}`);
+  }
+}
